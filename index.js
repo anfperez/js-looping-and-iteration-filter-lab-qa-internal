7d02a5bfa9712963afbab0b2923d875ddf96a490
Angela Perez <angela.perez@flatironschool.com>
2018-08-14 01:16:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
 
-
-function driversWithRevenueOver (drivers, revenue) {
-  return drivers.filter(function (driver) {
-    return driver.revenue > revenue;
+function findMatching (list, name) {
+  return list.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
   });
 }
 
-function driverNamesWithRevenueOver (drivers, revenue) {
-  return driversWithRevenueOver(drivers, revenue)
-    .map(function (driver) {
-      return driver.name;
-    });
-}
-
-function exactMatch (drivers, matcher) {
-  return drivers.filter(function (driver) {
-    let matches = false;
-
-    for (const key in matcher) {
-      matches = driver[key] === matcher[key];
-    }
-
-    return matches;
+function fuzzyMatch (list, partialName) {
+  let lengthOfName = partialName.length;
+  return list.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === partialName;
   });
 }
 
-function exactMatchToList (drivers, matcher) {
-  return exactMatch(drivers, matcher)
-    .map(function (driver) {
-      return driver.name;
-    });
+function matchName (list, name) {
+  return list.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
 }
